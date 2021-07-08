a6ca39bb2da81e48a2aae8405d0877f773b3d084
sriramjhansi <jhansisriram22@gmail.com>
2021-07-08 16:29:56 +0530

    first commit

---
+++ src/arrayclass.java
@@ -0,0 +1,25 @@
+
+
+public class arrayclass 
+{
+	
+	public void display()
+	{
+		//int arr[]=new int[];//here we will get complile time error
+		int arr[]=new int[5];
+		System.out.println(arr.length);//length of the array
+		String aa="jhansi";
+		System.out.println(aa.length());
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		arrayclass cc=new arrayclass();
+		cc.display();
+		
+		
+	}
+	
+
+}
